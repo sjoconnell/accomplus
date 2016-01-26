2e572a697757f2481604a0b7c2272bfc3d0f8423
Sam O'Connell <sjo773@gmail.com>
2016-01-26 10:02:31 -0600

    added backed to about page

--- templates/about-us.php
+++ templates/about-us.php
@@ -7,8 +7,8 @@
             <div class="banner-rhombus"></div>
             <div class="container">
                 <div class="banner-text-about">
-                    <h2>About Us</h2>
-                    <p>People helping people is our core philosophy at Accomplus.<br>Spanning advocacy, member services, regulatory and<br>governmental affairs, educational development and regional<br>management, our expertise is here to free up credit unions'<br>operating environment.</p>
+                    <h2><?php the_field('about_banner_title');?></h2>
+                    <p><?php the_field('about_banner_text');?></p>
                 </div>
             </div>
         </div>
@@ -16,45 +16,25 @@
         <div class="page-content">
             <div class="container">
                 <div class="about-text">
-                    <p>Accomplus is part of the Illinois Credit Union League. It was formed to better help credit unions in Illinois and beyond stay competitive and efficient in the constantly evolving compliance environment.</p>
+                    <p><?php the_field('about_main_text');?></p>
                 </div>
                 <div class="about-profiles">
                     <ul>
+                    <?php while( have_rows('about_profiles') ): the_row(); ?>
                         <li>
-                            <div class="about-profiles-image"></div>
+                            <div class="about-profiles-image" style="background-image:url(<?= the_sub_field('image'); ?>)"></div>
                             <div class="about-profiles-text">
-                                <h1>Stephen R. Olson</h1>
-                                <h2>EVP & General Counsel</h2>
-                                <p>Steve serves as executive vice president, general counsel and chief operating officer of Accomplus and the Illinois Credit Union League. In this role, he coordinates all dues-supported activities and directs all corporate legal affairs for the affiliated companies comprising the ICU System...</p>
+                                <h1><?php the_sub_field('name');?></h1>
+                                <h2><?php the_sub_field('job');?></h2>
+                                <p><?php the_sub_field('description');?></p>
                                 <div class="about-profile-text-hidden">
-                                    <p>Steve participated in the financial services industry coalition that obtained passage of the Illinois version of the Uniform Commercial Code Revised Article 9. He also drafted amendments to the Vehicle Code that abolished repossession titles in Illinois. In response to the residential mortgage foreclosure crisis that began in 2008 and escalated through 2013, he participated in financial services industry coalition efforts to negotiate reasonable responses to legislative initiatives amending the Illinois foreclosure law. He has drafted and testified in support of countless other measures amending numerous acts and laws that impact financial institution operations.</p>
-                                    <p>Prior to joining Accomplus and ICUL, Steve was in private practice in Galesburg, primarily involved in general corporate, municipal, commercial, bankruptcy and consumer finance law. He served on the Commercial Banking and Bankruptcy Law Section Council of the Illinois State Bar Association (1996–2008) and as its chairman (2006–2007), as well as its legislative committee chair.</p>
-                                    <p>He graduated with honors from St. Olaf College and received his law degree from the University of Iowa. He is licensed to practice in Illinois and Iowa, and is a member of the Illinois and Iowa State Bar Associations and the American Bankruptcy Institute. He speaks frequently to various bar and creditor groups on secured transactions, default remedies and bankruptcy matters.</p>
+                                    <p><?php the_sub_field('description_hidden');?></p>
                                 </div>
                                 <div class="about-profile-text-bottom"><p>read more</p></div>
                                 <div class="about-profile-text-bottom"><p>contact</p></div>
                             </div>
                         </li>
-                        <li>
-                            <div class="about-profiles-image"></div>
-                            <div class="about-profiles-text">
-                                <h1>Stephen R. Olson</h1>
-                                <h2>EVP & General Counsel</h2>
-                                <p>Steve serves as executive vice president, general counsel and chief operating officer of Accomplus and the Illinois Credit Union League. In this role, he coordinates all dues-supported activities and directs all corporate legal affairs for the affiliated companies comprising the ICU System...</p>
-                                <div class="about-profile-text-bottom"><a href="#">read more</a></div>
-                                <div class="about-profile-text-bottom"><a href="#">contact</a></div>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="about-profiles-image"></div>
-                            <div class="about-profiles-text">
-                                <h1>Stephen R. Olson</h1>
-                                <h2>EVP & General Counsel</h2>
-                                <p>Steve serves as executive vice president, general counsel and chief operating officer of Accomplus and the Illinois Credit Union League. In this role, he coordinates all dues-supported activities and directs all corporate legal affairs for the affiliated companies comprising the ICU System...</p>
-                                <div class="about-profile-text-bottom"><a href="#">read more</a></div>
-                                <div class="about-profile-text-bottom"><a href="#">contact</a></div>
-                            </div>
-                        </li>
+                    <?php endwhile; ?>
                     </ul>        
                 </div>
             </div>
